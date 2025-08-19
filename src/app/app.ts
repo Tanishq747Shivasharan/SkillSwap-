f9e72cb5f089a827fe1c6ee4b870f5d10be25152
Tanishq Shivasharan <155131699+Tanishq747Shivasharan@users.noreply.github.com>
2025-08-19 15:43:59 +0530

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('SkillSwap');
+  showWelcome = signal(true);
+
+  dismissWelcome() {
+    this.showWelcome.set(false);
+  }
+}
